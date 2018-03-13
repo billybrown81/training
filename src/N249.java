ef831cec166620f44bb7600fa855982e324b68a7
Billy Yang <billieyang81@gmail.com>
2018-03-12 23:40:27 -0400

    Mar12

---
+++ src/N249.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class N249 {
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> res = new ArrayList<>();
+        if (strings.length == 0) return res;
+        boolean isFound = false;
+        for (String s : strings) {
+        		for (List<String> list : res) {
+				String tmp = list.get(0);
+				if (isShift(s, tmp)) {
+					list.add(s);
+					isFound = true;
+					break;
+				}
+			}
+        		if (!isFound) {
+					List<String> list = new ArrayList<>();
+					list.add(s);
+					res.add(list);
+				}
+        		isFound = false;
+		}
+        return res;
+    }
+
+	private boolean isShift(String s, String tmp) {
+		if (s.length() == tmp.length()) {
+			for (int i = 1; i < s.length(); i++) {
+				if ((s.charAt(i) - s.charAt(i - 1) + 26) % 26 != (tmp.charAt(i) - tmp.charAt(i - 1) + 26) % 26) return false;
+			}
+			return true;
+		}
+		return false;
+	}
+}
